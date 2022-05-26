8f4a66e155548dcc0001dcaf80cbf9c77b76ae69
yAnselmus <eeromakinen@hotmail.com>
2022-05-26 15:32:55 +0300

    final commit

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
-const getAll asdf = async () => {
+const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
